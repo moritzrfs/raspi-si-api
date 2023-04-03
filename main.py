3b8ae5b908e81e38e30f5c2daf1d80f108e83420
Moritz <moritz.rfs@gmail.com>
2023-04-03 14:03:33 +0200

    Improve json checking

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException, Depends, Header, File, UploadFile
 from fastapi.security.api_key import APIKeyHeader, APIKey
 from starlette.status import HTTP_403_FORBIDDEN
 import os
+import json
 from typing import Union
 from utils.kill_proc import kill_proc
 from utils.start_proc import start_proc
@@ -62,7 +63,13 @@ async def create_upload_file(file: Union[UploadFile, None] = None, api_key: APIK
     if not file.filename.endswith('.json'):
         raise HTTPException(status_code=400, detail="Invalid file type. Only JSON files are allowed.")
 
+    contents = await file.read()
+    try:
+        json.loads(contents.decode('utf-8'))
+    except (json.JSONDecodeError, UnicodeDecodeError):
+        raise HTTPException(status_code=400, detail="Invalid file encoding. Only UTF-8 encoded JSON files are allowed.")
+
     file_path = os.getcwd() + '/' + file.filename
     with open(file_path, "wb") as buffer:
-        buffer.write(await file.read())
+        buffer.write(contents)
     return {"filename": file.filename}
